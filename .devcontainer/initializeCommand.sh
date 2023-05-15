2a2b05dbf0f76d66310a158bdec7d0e19fe2f6a7
Damon McCullough <damonmcc1391@gmail.com>
2023-05-15 01:34:35 +0000

    better comments

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -11,9 +11,11 @@ touch $SCRIPT_DIRECTORY/.env
 
 # only do this when running locally (rather than in a github action)
 if [[ ${CI} != "true" ]]; then
-	echo "Adding local SSH keys ..."
-	# Add local SSH private keys in order to push to github from the dev container
+	# Avoiding Dubious Ownership in Dev Containers for git
+	echo "Setting directiory as safe for git ..."
 	git config --global --add safe.directory $PWD
+	# echo "Adding local SSH keys ..."
+	# Add local SSH private keys in order to push to github from the dev container
 	# ssh-add
 fi
 
