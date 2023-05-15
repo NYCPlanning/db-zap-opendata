7a156b3a4e9a4fd1f837f5f9df019eace1f63775
Damon McCullough <damonmcc1391@gmail.com>
2023-05-15 02:00:34 +0000

    fix column name

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,6 +5,6 @@ TEST_DATA_PATH = "tests/test_data"
 
 def test_timestamp_to_date():
     data = pd.read_csv(f"{TEST_DATA_PATH}/timestamp_data.csv")
-    data_dates = timestamp_to_date(data, date_columns=["date_column_input"])
+    data_dates = timestamp_to_date(data, date_columns=["date_column_a"])
 
     assert data_dates["date_column_a"].equals(data_dates["date_column_a_expected"])
