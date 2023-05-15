08515b1530a737c814479b057e926846b25bdff7
Damon McCullough <damonmcc1391@gmail.com>
2023-05-15 15:04:46 +0000

    use pyproject for requrements

--- bash/dev_compile_python_packages.sh
+++ bash/dev_compile_python_packages.sh
@@ -6,10 +6,13 @@ RELATIVE_SCRIPTPATH=$0
 
 # Update and install packages used to compile requirements
 python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade pip-tools wheel
+python3 -m pip install --upgrade pip-tools wheel validate-pyproject
+
+# Validate pyproject.toml
+validate-pyproject pyproject.toml
 
 # Delete existing requirements file to ensure full dependency resolution
 rm -rf requirements.txt
 
 # Compile requirements
-CUSTOM_COMPILE_COMMAND="${RELATIVE_SCRIPTPATH}" python3 -m piptools compile --output-file=requirements.txt requirements.in
+CUSTOM_COMPILE_COMMAND="${RELATIVE_SCRIPTPATH}" python3 -m piptools compile --output-file=requirements.txt pyproject.toml
