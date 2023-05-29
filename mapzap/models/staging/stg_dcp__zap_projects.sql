b27866f989410e2c08c650270161df7c5907a594
Damon McCullough <damonmcc1391@gmail.com>
2023-05-29 17:31:30 +0000

    use recoded columns

--- mapzap/models/staging/stg_dcp__zap_projects.sql
+++ mapzap/models/staging/stg_dcp__zap_projects.sql
@@ -7,30 +7,31 @@ with source as (
 zap_projects as (
 
     select
-        dcp_projectid as project_id,
-        dcp_projectname as project_name,
-        dcp_name as project_code,
-        dcp_certifiedreferred as project_certified_referred_date,
+        project_id,
+        project_name,
+        certified_referred as project_certified_referred_date,
+        -- new columns
+        applicant_type,
+        ulurp_numbers,
+        ulurp_non as ulurp_type,
+        ceqr_number,
+        ceqr_type,
+        project_status,
+        public_status,
+        actions as action_codes,
         EXTRACT(
             isoyear
             from
-            dcp_certifiedreferred
-        ) as project_certified_referred_year,
-        -- new columns
-        dcp_applicanttype as applicant_type,
-        dcp_ulurpnumbers as ulurp_numbers,
-        dcp_ceqrnumber as ceqr_number,
-        dcp_ceqrtype as ceqr_type,
-        dcp_leaddivision as lead_division,
-        statuscode as project_status,
-        dcp_publicstatus as public_status,
-        dcp_actionnumbers as action_numbers,
-        dcp_currentzoningdistrict as current_zoning_district,
-        dcp_proposedzoningdistrict as proposed_zoning_district,
-        dcp_wrpreviewrequired as wrp_review_required,
-        dcp_femafloodzonecoastala as fema_flood_zone_coastal,
-        dcp_femafloodzonev as fema_flood_zone_v
-        -- more to add
+            certified_referred
+        ) as project_certified_referred_year
+
+        -- not in recoded data
+        {# dcp_leaddivision as lead_division, #}
+        {# dcp_currentzoningdistrict as current_zoning_district, #}
+        {# dcp_proposedzoningdistrict as proposed_zoning_district, #}
+        {# dcp_wrpreviewrequired as wrp_review_required, #}
+        {# dcp_femafloodzonecoastala as fema_flood_zone_coastal, #}
+        {# dcp_femafloodzonev as fema_flood_zone_v #}
     from source
 )
 
