22f00f814bb19bb4c5486114c9c7eba3d2a6c227
Damon McCullough <damonmcc1391@gmail.com>
2023-06-01 18:26:28 +0000

    don't need this

--- src/__init__.py
+++ src/__init__.py
@@ -15,6 +15,3 @@ ZAP_ENGINE = os.environ["ZAP_ENGINE"]
 base_path = ".output"
 if not os.path.isdir(base_path):
     os.makedirs(base_path, exist_ok=True)
-    # create .gitignore so that files in this directory aren't tracked
-    with open(f"{base_path}/.gitignore", "w") as f:
-        f.write("*")
