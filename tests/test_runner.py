0f8b5556ef18958dd464e8addd8cbc0b82ebc62f
Damon McCullough <damonmcc1391@gmail.com>
2023-06-01 18:42:53 +0000

    use runner in test

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,5 +1,10 @@
 import pytest
+from src.runner import Runner
+
+test_dataset_name = "does_not_exist"
+
 
 @pytest.mark.integration()
 def test_runner():
-    assert True
+    runner = Runner(name=test_dataset_name)
+    runner()
