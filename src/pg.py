5b10c38621ba26f20963637fe02d1e386d3d3699
Damon McCullough <damonmcc1391@gmail.com>
2023-06-02 14:47:45 +0000

    expand use of PG class

--- src/pg.py
+++ src/pg.py
@@ -1,8 +1,49 @@
 import pandas as pd
-from sqlalchemy import create_engine
+import string
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import ProgrammingError
+from psycopg2.extensions import AsIs
+
+
+def format_sql_query_parameters(parameters: dict) -> dict:
+    formatted_parameters = {}
+    for key, value in parameters.items():
+        formatted_parameters[key] = AsIs(value)
+    return formatted_parameters
 
 
 class PG:
-    def __init__(self, url: str):
+    def __init__(self, url: str, schema: str):
         self.url = url
-        self.engine = create_engine(url)
+        self.schema = schema
+        self.engine = create_engine(
+            url,
+            isolation_level="AUTOCOMMIT",
+            connect_args={"options": "-csearch_path={}".format(self.schema)},
+        )
+        self.create_schema()
+
+    def create_schema(self) -> None:
+        try:
+            self.execute_sql_query(
+                "CREATE SCHEMA :schema", {"schema": AsIs(self.schema)}
+            )
+            print(f"Schema '{self.schema}' created")
+        except ProgrammingError:
+            print(f"Schema '{self.schema}' already exists")
+
+    def execute_select_query(self, base_query: str, parameters: dict) -> pd.DataFrame:
+        with self.engine.begin() as sql_connection:
+            records = pd.read_sql(
+                sql=text(base_query),
+                con=sql_connection,
+                params=format_sql_query_parameters(parameters),
+            )
+        return records
+
+    def execute_sql_query(self, base_query: str, parameters: dict) -> None:
+        with self.engine.connect() as connection:
+            connection.execute(
+                statement=text(base_query),
+                parameters=format_sql_query_parameters(parameters),
+            )
