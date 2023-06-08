a70b2c9bae084346d958490c82f2fb595fbca203
Damon McCullough <damonmcc1391@gmail.com>
2023-06-08 18:58:51 +0000

    don't skip recode_id test

--- tests/test_recode_id.py
+++ tests/test_recode_id.py
@@ -6,26 +6,51 @@ from src.recode_id import recode_id
 
 test_data_input = pd.DataFrame.from_dict(
     {
-        # "crm_project_id": ["a99a916a-1133-e811-8126-1458d04dc8c8", "id_2"],
-        "crm_project_id": ["046f7ed3-6197-e911-a98a-001dd83080ab", "046f7ed3-24-more-text"],
-        "primary_applicant": ["applicant_1", "applicant_2"],
-        "ceqr_leadagency": ["agency_1", "agency_2"],
-        "current_milestone": ["milestone_1", "milestone_2"],
-        "current_envmilestone": ["envmilestone_1", "envmilestone_2"],
+        "crm_project_id": [
+            "046f7ed3-6197-e911-a98a-001dd83080ab",
+            "046f7ed3-24-more-text",
+        ],
+        "primary_applicant": [
+            "applicant_1",
+            "applicant_2",
+        ],
+        "ceqr_leadagency": [
+            "agency_1",
+            "agency_2",
+        ],
+        "current_milestone": [
+            "milestone_1",
+            "milestone_2",
+        ],
+        "current_envmilestone": [
+            "envmilestone_1",
+            "envmilestone_2",
+        ],
     }
 )
 
 recoded_data_expected = pd.DataFrame.from_dict(
     {
-        # "crm_project_id": ["046f7ed3-6197-e911-a98a-001dd83080ab", "id_2"],
-        "primary_applicant": ["624 Morris B, LLC", "applicant_2"],
-        "ceqr_leadagency": ["DCP", "agency_2"],
-        "current_milestone": ["EAS - Project Completed  ", "milestone_2"],
-        "current_envmilestone": ["EAS - Review Filed EAS  ", "envmilestone_2"],
+        "primary_applicant": [
+            "624 Morris B, LLC",
+            "applicant_2",
+        ],
+        "ceqr_leadagency": [
+            "DCP",
+            "agency_2",
+        ],
+        "current_milestone": [
+            "EAS - Project Completed  ",
+            "milestone_2",
+        ],
+        "current_envmilestone": [
+            "EAS - Review Filed EAS  ",
+            "envmilestone_2",
+        ],
     }
 )
 
-@pytest.mark.skip()
+
 def test_recode_id():
     recoded_data_actual = recode_id(test_data_input)
     pd.testing.assert_frame_equal(recoded_data_actual, recoded_data_expected)
